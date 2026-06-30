f7e23eb817473202f0bafdd96c7f935c47024526
Youllia <888youll@gmail.com>
2026-07-01 00:55:51 +0200

    q.details fix fi, answer

--- src/app/features/question-detail/question-detail.ts
+++ src/app/features/question-detail/question-detail.ts
@@ -7,6 +7,7 @@ import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-question-detail',
@@ -17,7 +18,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatRadioGroup, 
     MatButtonModule, 
     MatButtonToggleModule, 
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './question-detail.html',
   styleUrl: './question-detail.scss',
@@ -26,38 +28,29 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 export class QuestionDetail implements OnInit {
   #route = inject(ActivatedRoute);
   store = inject(QuestionStore);
-  #id = signal(0);
-
-
   protected mode = inject(ModeState);
-  //protected unlock = signal(false);
+  
+  #id = signal<number>(0);
 
   protected question = computed(() => this.store.questions()
-    .find(q => q.id === this.#id()));
-
-  protected selectedIds = signal<number[]>([]);
+  .find(q => q.id === this.#id()));
   
-  protected toggleSelected(id: number, checked: boolean) {
-    const current = this.selectedIds();
-    this.selectedIds.set(
-      checked ? [...current, id] : current.filter(x => x !== id)
-    );
-  }
-
+  protected selectedAnswerId = signal<number | null>(null);
+  protected selectedAnswerIds = signal<number[]>([]);
   protected userInput = signal('');
 
-
+  checked  = signal<boolean>(false);
+  
   ngOnInit() {
     this.#route.paramMap.subscribe({
       next: params => {
         this.#id.set(Number(params.get('questionId')));
-
-        this.selectedId.set(null);
-        this.checked.set(false);
-        //this.unlock.set(false);
-        this.selectedIds.set([]);
+        
+        this.selectedAnswerId.set(null);
+        this.selectedAnswerIds.set([]);
         this.userInput.set('');
-
+        this.checked.set(false);
+        
         const catId = Number(params.get('catalogId'));
         if (this.store.questions().length === 0) {
           this.store.loadByCatalog(catId);
@@ -65,38 +58,64 @@ export class QuestionDetail implements OnInit {
       }
     });
   }
+  
 
 
   // Antwortcheck
-  selectedId = signal<number | null>(null);
-  checked = signal(false);
-
   isCorrect = computed(() => {
     const q = this.question();
     if (!q) return false;
 
-    const correctIds = q.answers.filter(a => a.isCorrect).map(a => a.id).sort();
-  });
+    const correctIds = q.answers
+    .filter(a => a.isCorrect)
+    .map(a => a.id).sort();
 
- /*  isCorrect = computed(() => this.question()?.answers
-    .find(a => a.id === this.selectedId())?.isCorrect ?? false); */
+    if (q.type === 'sc') {
+      return correctIds.length === 1 
+      && correctIds[0] === this.selectedAnswerId();
+    }
+  
+    if (q.type === 'mc') {
+      const selected = [...this.selectedAnswerIds()].sort();
+      return selected.length === correctIds.length 
+      && selected.every((id, i) => id === correctIds[i]);
+    }
+
+    if (q.type === 'fi') {
+      const user = this.userInput().trim().toLocaleLowerCase();
+      if (!user) return false;
+      return q.answers.some(a => a.isCorrect && 
+        a.answerText.trim().toLowerCase() === user);
+    }
+    return false;
+  });
 
   correctAnswers = computed(() => this.question()?.answers
     .filter(a => a.isCorrect));
+  
+  
+    
 
+  protected toggleSelected(id: number, checked: boolean) {
+    const current = this.selectedAnswerIds();
+  
+    this.selectedAnswerIds.set(
+      checked ? [...current, id] : current.filter(x => x !== id)
+    );
+  }
 
-
+      
   // Navigation zwischen Fragen
   protected prevId = computed(() => {
-    const qArr = this.store.questions();
-    const i = qArr.findIndex(q => q.id === this.#id());
-    return (i > 0) ? qArr[i - 1]?.id : undefined
+    const questions = this.store.questions();
+    const i = questions.findIndex(q => q.id === this.#id());
+    return (i > 0) ? questions[i - 1]?.id : undefined
   });
   
   protected nextId = computed(() => {
-    const qArr = this.store.questions();
-    const i = qArr.findIndex(q => q.id === this.#id());
-    return (i >= 0 && i < qArr.length - 1 ) ? qArr[i + 1]?.id : undefined;
+    const questions = this.store.questions();
+    const i = questions.findIndex(q => q.id === this.#id());
+    return (i >= 0 && i < questions.length - 1 ) ? questions[i + 1]?.id : undefined;
   });
 
 
