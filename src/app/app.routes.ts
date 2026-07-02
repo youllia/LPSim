e972d6638dffb12d529e59b717f8f1d89ed01f3a
youllia <888youll@gmail.com>
2026-07-02 15:17:43 +0200

    routes, nf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,17 @@ import { CatalogSelect } from './features/catalog-select/catalog-select';
 import { QuestionSelect } from './features/question-select/question-select';
 import { QuestionDetail } from './features/question-detail/question-detail';
 import { ModeSelect } from './features/mode-select/mode-select';
+import { Anleitung } from './features/anleitung/anleitung';
+import { NotFound } from './features/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'topics', component: TopicSelect },
+  { path: 'anleitung', component: Anleitung },
+  { path: 'topics',  redirectTo: '', pathMatch: 'full' },
   { path: 'topics/:topicId/catalogs', component: CatalogSelect },
   { path: 'catalogs/:catalogId/questions', component: QuestionSelect },
   { path: 'catalogs/:catalogId/questions/:questionId', component: QuestionDetail },
-  { path: 'mode', component: ModeSelect }
+  { path: 'mode', component: ModeSelect },
+  { path: '**', component: NotFound}
 ];
 
---
+++ src/app/features/anleitung/anleitung.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anleitung',
+  imports: [],
+  templateUrl: './anleitung.html',
+  styleUrl: './anleitung.scss',
+})
+export class Anleitung {}
